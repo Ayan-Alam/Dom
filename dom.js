8107c2303efd117e3f37ad16ddefdbc810e8f676
Ayan-Alam <ayanalam186@gmail.com>
2023-06-01 17:39:19 +0530

    "Creating Nodes and Modifying Dom"

--- dom.js
+++ dom.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
         myElement1[i].style.fontWeight = 'bold';
     }
     var myElement2 = document.getElementsByTagName('li');
-    myElement2[4].style.backgroundColor = "grey"; 
     var item = document.querySelectorAll('li');
     item[1].style.color = 'green';
     var item2 = document.querySelector('.list-group-item:nth-child(3)');
@@ -15,6 +14,26 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var i=0;i<item3.length;i++){
         item3[i].style.backgroundColor = 'green';
     }
+    // Traversing The DOM
+    var itemList = document.querySelector('#items');
+    console.log(itemList.parentElement);
+    console.log(itemList.lastElementChild);
+    console.log(itemList.lastChild);
+    console.log(itemList.firstChild);
+    console.log(itemList.nextSibling);
+    console.log(itemList.nextElementSibling);
+    console.log(itemList.previousSibling);
+    console.log(itemList.previousElementSibling);
+
+    var newDiv = document.createElement('div');
+    console.log(newDiv);
+    newDiv.setAttribute('class','divelement');
+    var someText = document.createTextNode('hello world');
+    newDiv.appendChild(someText);
+    var head = document.querySelector('#head');
+    var newel = document.createElement('p');
+    newel.appendChild(someText);
+    head.appendChild(newel);
   });
 
    
