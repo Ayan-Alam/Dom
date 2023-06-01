29b8aa79e2c6eb221f972517d319b53b578aa578
Ayan-Alam <ayanalam186@gmail.com>
2023-06-01 15:54:13 +0530

    "use QuerySelector and QuerySelectorAll"

--- dom.js
+++ dom.js
@@ -5,9 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var i=0;i<4;i++){
         myElement1[i].style.fontWeight = 'bold';
     }
-    myElement1[2].style.backgroundColor = 'green';
     var myElement2 = document.getElementsByTagName('li');
     myElement2[4].style.backgroundColor = "grey"; 
+    var item = document.querySelectorAll('li');
+    item[1].style.color = 'green';
+    var item2 = document.querySelector('.list-group-item:nth-child(3)');
+    item2.style.visibility = "hidden";
+    var item3 = document.querySelectorAll('li:nth-child(odd)');
+    for(var i=0;i<item3.length;i++){
+        item3[i].style.backgroundColor = 'green';
+    }
   });
 
    
